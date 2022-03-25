6dbf5fa368f27293be481b5a66a11f2ba49fe61c
Elocater <elocaters.notes@gmail.com>
2022-03-25 01:58:16 -0700

    setup preview links

--- src/templates/note.tsx
+++ src/templates/note.tsx
@@ -5,8 +5,13 @@ import { MDXProvider } from '@mdx-js/react';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
 import Notice from '../components/notice';
 import { Helmet } from 'react-helmet';
-import * as noteStyles from './note.module.css';
 import Backlinks from '../components/backlinks';
+import PreviewLink from '../components/preview_link';
+import NoteTitle from '../components/note_title';
+import Tippy from '@tippyjs/react';
+
+import * as noteStyles from './note.module.css';
+import 'tippy.js/dist/tippy.css';
 
 interface QueryData {
   site: {
@@ -25,13 +30,31 @@ interface QueryData {
     backlinks: [
       { slug: string; frontmatter: { title: string }; excerpt: string },
     ];
+    links: [
+      {
+        slug: string;
+        frontmatter: {
+          title: string;
+        };
+        body: string;
+      },
+    ];
   };
 }
 
-const shortcodes = { Notice };
-
 const NotePage = (context: any) => {
   let data: QueryData = context.data;
+
+  const mdx_components = {
+    Notice,
+    a: ({ href, children }: { href: string; children: JSX.Element[] }) => {
+      return (
+        <PreviewLink links_on_page={data.mdx.links} href={href}>
+          {children}
+        </PreviewLink>
+      );
+    },
+  };
   return (
     <Layout>
       <Helmet>
@@ -39,7 +62,9 @@ const NotePage = (context: any) => {
         <title>{data.mdx.frontmatter.title}</title>
       </Helmet>
       <article className={noteStyles.main}>
-        <h1 className={noteStyles.title}>{data.mdx.frontmatter.title}</h1>
+        <h1 style={{ marginBottom: '0rem' }}>
+          <NoteTitle>{data.mdx.frontmatter.title}</NoteTitle>
+        </h1>
 
         <section className={noteStyles.metadataHeader}>
           <p>{data.mdx.timeToRead} min read</p>
@@ -47,7 +72,7 @@ const NotePage = (context: any) => {
         </section>
 
         <section>
-          <MDXProvider components={shortcodes}>
+          <MDXProvider components={mdx_components}>
             <MDXRenderer>{data.mdx.body}</MDXRenderer>
           </MDXProvider>
         </section>
@@ -67,6 +92,13 @@ export const query = graphql`
       }
     }
     mdx(id: { eq: $id }) {
+      links {
+        slug
+        frontmatter {
+          title
+        }
+        body
+      }
       backlinks {
         slug
         frontmatter {
